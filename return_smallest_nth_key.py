ea09445694651aaf2bab0b586b147d6fbe56c7f6
Therin Young <therin.young@gmail.com>
2022-08-28 23:48:58 -0400

    Add files via upload
    This is a python practice problem from Facebook Meta preparation HUB. You are given an input dictionary with keys as strings, and values as integers. Complete a function that returns the key with the nth smallest value. If the solution involves two keys that have the same value, return the key that is lexicographically earliest. If n is larger than the number of distinct keys or equal to 0, then return null.

---
+++ return_smallest_nth_key.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 28 23:43:21 2022
+
+@author: theri
+"""
+import math
+# Add any extra import statements you may need here
+# Add any helper functions you may need here
+
+def return_smallest_key(inputDict, n):
+  
+  
+  
+  
+  # extract keys from input dictionary
+  lstKeys = list(inputDict.keys())
+  
+  # sort keys from input dictionary
+  #lstKeys.sort()
+
+  # extract values from input dictionary
+  lstValues = list(inputDict.values())
+  
+  if n > len(lstValues):
+    return (None)
+  
+  elif n == 0:
+    return (None)
+  
+  else:
+
+    n = n-1
+    
+    # create empty set to store values once they've been seen
+    seen = set()
+
+    # create empty list to store duplicate values from input dictionary
+    dupes = []
+
+    # find duplicate values
+    for value in lstValues:
+        if value in seen:
+            dupes.append(value)
+        else:
+            seen.add(value)
+
+
+
+    value_lst = [] # to store dict values
+    key_lst = [] # to store duplicate keys
+
+    for key in lstKeys:
+      result = inputDict[key]
+      if result in dupes:
+        key_lst.append(key)
+        value_lst.append(result)
+      else:
+        value_lst.append(result)
+
+    #sort value_lst and key_lst
+    value_lst.sort()
+    key_lst.sort()
+
+    #find the nth value
+    nth_value = value_lst[n] #nth value is a number but we don't know what the key is
+
+    #create new dict that swaps value and keys from input dict
+    new_dict = {}
+
+    for i in range(len(lstKeys)):
+      new_dict.update({lstValues[i]:lstKeys[i]})
+
+    #find key that corresponds to nth value
+    nkey = new_dict[nth_value]
+
+    if nkey not in key_lst:
+      answer = nkey
+    else:
+      answer = key_lst[0]
+    
+    return(answer)
